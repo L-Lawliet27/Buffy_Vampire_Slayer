4f8629f932b1d9a31cec09c6e97bba9cf4975df9
Andres Salazar Colina <andressalazarcolina@gmail.com>
2020-12-13 20:50:58 +0100

    Dracula didn't have a problem + Fixed Light Attack

--- tp1/gameElements/Attackers/Vampire.java
+++ tp1/gameElements/Attackers/Vampire.java
@@ -103,6 +103,7 @@ public class Vampire extends Attacker {
     @Override
     public void receiveLightFlashAttack() {
         receiveSlayerAttack(lives);
+        vampiresOnBoard--;
     }
 
     @Override
